954c010388f9d16e09a61daa40eea7dae56f6ffc
Vihang Kale <59720809+vihangkale@users.noreply.github.com>
2021-04-30 20:39:20 +0530

    Create CountWordString.js

---
+++ CountWordString.js
@@ -0,0 +1,11 @@
+//Write a program to count total number of words in a string.
+
+let string = "Liverpool is the best football  club in the  world";
+
+function Count() {
+  
+	return string.split(' ').length; 
+	
+}
+
+Count();
