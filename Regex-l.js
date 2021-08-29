11300831d54747cc9d33a76f79bc1078f16df38b
Vihang Kale <59720809+vihangkale@users.noreply.github.com>
2021-08-29 15:52:06 +0530

    Update Regex-l.js

--- Regex-l.js
+++ Regex-l.js
@@ -1,12 +1,12 @@
 function regexVar() {
     /*
+       create a regex where the string/word starts with vowel and ends with same vowel also consonent in middle of both of them 
      * Declare a RegExp object variable named 're'
-     * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
+    
+    * It must match a string that starts and ends with the same vowel (i.e., {a, e, i, o, u})
      */
     
     let re =  new RegExp('^([aeiou]).+\\1$');
-    /*
-     * Do not remove the return statement
-     */
+
     return re;
 }
